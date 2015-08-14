0376ea361a8c6ca130e42f9624088a1bf15db4f5
Bunker <Michaelbunker1@gmail.com>
2015-08-14 10:15:50 -0700

    add initialize method to Word Class.

--- lib/word.rb
+++ lib/word.rb
@@ -1,3 +1,18 @@
 class Word
 
+  @@words = []
+  @@id_count = 0
+
+  attr_reader :word, :definition, :id
+
+  define_method(:initialize) do |attributes|
+    @word = attributes.fetch(:word)
+    @id = @@id_count + 1
+    @@id_count += 1
+
+    @definitions = []
+  end
+
+  
+
 end
