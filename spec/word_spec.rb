cd2d0ac97429a4fc249643c8b46a68f0e285bcde
Bunker <Michaelbunker1@gmail.com>
2015-08-14 09:53:27 -0700

    add .all spec to the word Class

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -4,11 +4,18 @@ require('definition')
 
 describe(Word) do
 
+  describe('.all') do
+    it('returns the word array empty') do
+      expect(Word.all()).to(eq([]))
+    end
+  end
+
   describe('#word') do
     it('returns a word for the user') do
       new_word = Word.new({:word => 'autonomous'})
       expect(new_word.word()).to(eq('autonomous'))
     end
   end
+
 
 end
