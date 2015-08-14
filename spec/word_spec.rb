566e3c82f51c8dc04aeaf82b7efa0098f6a961bc
Bunker <Michaelbunker1@gmail.com>
2015-08-14 10:06:48 -0700

    add #add_definition spec for the Word class.

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -12,7 +12,7 @@ describe(Word) do
 
   describe('.clear') do
     it('clears words array of entries') do
-      new_word = Word.new({:word => 'justice'})
+      new_word = Word.new({:word => 'JFK', :definition => 'political leader'})
       new_contact.save()
       Contacts.clear()
       expect(Contacts.all()).to(eq([]))
@@ -21,9 +21,9 @@ describe(Word) do
 
   describe('.find') do
     it('finds a word based on its id') do
-      new_word = Word.new({:word => 'harmony'})
+      new_word = Word.new({:word => 'harmony', :definition => 'peace'})
       new_word.save()
-      new_word2 = Word.new(:word => 'buddha')
+      new_word = Word.new({:word => 'buddha', :definition => 'religious leader'})
       new_word2.save()
       expect(Contacts.find(2)).to(eq(new_word2))
     end
@@ -31,16 +31,24 @@ describe(Word) do
 
   describe('#word') do
     it('returns a word for the user') do
-      new_word = Word.new({:word => 'autonomous'})
+      new_word = Word.new({:word => 'jesus', :definition => 'christian leader'})
       expect(new_word.word()).to(eq('autonomous'))
     end
   end
 
   describe('#save') do
     it('saves a word to the word array') do
-      new_word = Word.new({:word => 'josue'})
+      new_word = Word.new({:word => 'god', :definition => 'diety for religion'})
       new_word.save()
       expect(Word.all()).to(eq([new_word]))
+    end
+  end
+
+  describe('#add_definition') do
+    it('adds a definition to a word') do
+      new_word = Word.new({:word => 'muhammad', :definition => 'islam leader'})
+      new_word.save()
+      expect(new_word.add_definition('islam leader')).to(eq(['islam leader']))
     end
   end
 
