b07771eb56ee4faa11ae20ecb40ac505cb3c67df
Bunker <Michaelbunker1@gmail.com>
2015-08-14 10:58:12 -0700

    add integration spec for adding definition to word.

--- spec/word_integration_spec.rb
+++ spec/word_integration_spec.rb
@@ -1,6 +1,6 @@
 require('capybara/rspec')
 require('./app')
-Capybara.app = Sinatra::application
+Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
 describe('the add word path', {:type => :feature}) do
@@ -9,6 +9,16 @@ describe('the add word path', {:type => :feature}) do
     fill_in('word', :with => 'Hitchens')
     click_button('add word')
     expect(page).to have_content('Hitchens')
+  end
+
+  it('adds a definition to a word') do
+    visit('/')
+    fill_in('word', :with => 'islam')
+    click_button('add word')
+    click_link('islam')
+    fill_in('definition', :with => 'a religion')
+    click_button('add definition')
+    expect(page).to have_contect('a religion')
   end
 
 end
